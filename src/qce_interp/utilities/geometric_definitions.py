a370c57510ca5713479491afc8006929b3acd52d
SeanvdMeer <18538762+MiniSean@users.noreply.github.com>
2023-12-30 01:29:17 +0100

    Added plotting method for state classification

--- src/qce_interp/utilities/geometric_definitions.py
+++ src/qce_interp/utilities/geometric_definitions.py
@@ -2,7 +2,7 @@
 # Module containing vector definitions.
 # -------------------------------------------
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Tuple, List
 import numpy as np
 
 
@@ -33,3 +33,50 @@ class Vec2D:
             return Vec2D(x=self.x + other.x, y=self.y + other.y)
         raise NotImplemented(f"Addition with anything other than {Vec2D} is not implemented.")
     # endregion
+
+
+@dataclass(frozen=True)
+class Polygon:
+    """
+    Data class, containing Vec2D describing a polygon.
+    """
+    vertices: List[Vec2D]
+
+    # region Class Methods
+    def get_convex_vertices(self) -> List[Vec2D]:
+        """
+        Orders a list of Vec2D points to form a non-self-intersecting polygon.
+        :return: List of Vec2D vertices ordered to form a non-self-intersecting polygon.
+        """
+
+        def centroid(points: List[Vec2D]) -> Vec2D:
+            """Calculates the centroid of a polygon given its vertices."""
+            x = [p.x for p in points]
+            y = [p.y for p in points]
+            centroid_x = sum(x) / len(points)
+            centroid_y = sum(y) / len(points)
+            return Vec2D(centroid_x, centroid_y)
+
+        def angle_from_centroid(point: Vec2D, centroid: Vec2D) -> float:
+            """Calculates the angle between a point and the centroid of the polygon."""
+            return np.arctan2(point.y - centroid.y, point.x - centroid.x)
+
+        # Calculate the centroid of the polygon
+        center = centroid(self.vertices)
+
+        # Sort the vertices based on their angle from the centroid
+        sorted_vertices = sorted(self.vertices, key=lambda point: angle_from_centroid(point, center))
+
+        return sorted_vertices
+    # endregion
+
+
+def euclidean_distance(p1: Vec2D, p2: Vec2D):
+    """
+    Calculates Euclidean distance between two points.
+
+    :param p1: First point.
+    :param p2: Second point.
+    :return: Euclidean distance between p1 and p2.
+    """
+    return np.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2)
